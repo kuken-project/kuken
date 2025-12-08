149e7628687b3c419acd7c7dad464044280d3bb1
Natan <natanvnascimento@gmail.com>
2025-12-08 14:48:15 -0300

    Add CompositeEventDispatcher to combine multiple event dispatchers

--- server/kuken/src/main/kotlin/gg/kuken/core/EventDispatcher.kt
+++ server/kuken/src/main/kotlin/gg/kuken/core/EventDispatcher.kt
@@ -6,6 +6,7 @@ import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.filterIsInstance
+import kotlinx.coroutines.flow.merge
 import kotlin.reflect.KClass
 
 interface EventDispatcher : CoroutineScope {
@@ -31,4 +32,19 @@ internal class EventDispatcherImpl :
     override fun dispatch(event: Any) {
         publisher.tryEmit(event)
     }
+}
+
+internal class CompositeEventDispatcher(
+    private val dispatchers: List<EventDispatcher>
+) : EventDispatcher {
+
+    override val coroutineContext = SupervisorJob() + Dispatchers.IO
+
+    override fun dispatch(event: Any) {
+        dispatchers.forEach { it.dispatch(event) }
+    }
+
+    override fun <T : Any> listen(eventType: KClass<T>): Flow<T> =
+        dispatchers.map { it.listen(eventType) }
+            .merge()
 }
