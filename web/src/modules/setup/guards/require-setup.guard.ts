6e64e1f7a59e5dcf23177312ec2ce3e8aed57291
Natan <natanvnascimento@gmail.com>
2025-12-17 15:32:36 -0300

    fix(web): streamline `RequireSetupGuard` logic and improve flow control

--- web/src/modules/setup/guards/require-setup.guard.ts
+++ web/src/modules/setup/guards/require-setup.guard.ts
@@ -1,46 +1,38 @@
-import type { NavigationGuard, NavigationGuardNext, RouteLocationNormalized } from "vue-router"
+import {
+    type NavigationGuard,
+    type NavigationGuardNext,
+    type RouteLocationNormalized
+} from "vue-router"
 import setupService from "@/modules/setup/api/services/setup.service.ts"
 import { AxiosError } from "axios"
 import { AUTH_LOGIN_ROUTE } from "@/modules/auth/auth.routes.ts"
 import { SETUP_ROUTE } from "@/router.ts"
-import { type Setup, SetupStepType } from "@/modules/setup/api/models/setup.model.ts"
 
 export const RequireSetupGuard: NavigationGuard = async (
     _to: RouteLocationNormalized,
     _from: RouteLocationNormalized,
     next: NavigationGuardNext
 ) => {
-    const comingFromLogin = _to.name == AUTH_LOGIN_ROUTE
-    let setup: Setup
+    if (_from.name == AUTH_LOGIN_ROUTE) {
+        return next()
+    }
 
     try {
-        setup = await setupService.getSetup()
+        await setupService.getSetup()
     } catch (error) {
         if (!(error instanceof AxiosError)) throw error
 
-        if (error.response?.status === 423 /* Locked */) {
-            return comingFromLogin ? next() : next({ name: AUTH_LOGIN_ROUTE })
+        const isDueToSetupCompleted = error.response?.status === 423 /* Locked */
+
+        if (isDueToSetupCompleted) {
+            const isGoingToSetupPage = _to.name == SETUP_ROUTE
+
+            // Trying to access setup but setup is already completed
+            return isGoingToSetupPage ? next("/") : next()
         }
 
-        console.error("Failed to get setup status", error)
-        return next(false)
+        return next(error)
     }
 
-    const isInSettingUp = _to.name == SETUP_ROUTE
-    if (setup.completed) {
-        if (isInSettingUp) return next()
-        else return next({ name: SETUP_ROUTE })
-    }
-
-    if (comingFromLogin) {
-        const needsToCreateAccount =
-            setup.remainingSteps.findIndex((step) => step.type === SetupStepType.CREATE_ACCOUNT) >=
-            0
-
-        if (needsToCreateAccount) {
-            return next({ name: SETUP_ROUTE })
-        }
-    }
-
-    return isInSettingUp ? next(true) : next({ name: SETUP_ROUTE })
+    return next({ name: SETUP_ROUTE })
 }
