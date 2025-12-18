3f0b3c3d66abc606a214ecb9972565e0f9568a54
Natan <natanvnascimento@gmail.com>
2025-12-18 17:09:42 -0300

    refactor(server/blueprints): replace `receiveValidating` with `receiveValid` in `importBlueprint` route

--- server/src/main/kotlin/gg/kuken/feature/blueprint/http/routes/ImportSpec.kt
+++ server/src/main/kotlin/gg/kuken/feature/blueprint/http/routes/ImportSpec.kt
@@ -3,19 +3,17 @@ package gg.kuken.feature.blueprint.http.routes
 import gg.kuken.feature.blueprint.BlueprintService
 import gg.kuken.feature.blueprint.BlueprintSpecSource
 import gg.kuken.feature.blueprint.http.BlueprintRoutes
-import gg.kuken.http.util.receiveValidating
+import gg.kuken.http.util.receiveValid
 import io.ktor.server.resources.post
 import io.ktor.server.response.respond
 import io.ktor.server.routing.Route
-import jakarta.validation.Validator
 import org.koin.ktor.ext.inject
 
 fun Route.importBlueprint() {
     val blueprintService by inject<BlueprintService>()
-    val validator by inject<Validator>()
 
     post<BlueprintRoutes.Import> {
-        val source = call.receiveValidating<BlueprintSpecSource>(validator)
+        val source = call.receiveValid<BlueprintSpecSource>()
         val blueprint = blueprintService.importBlueprint(source)
 
         call.respond(blueprint)
