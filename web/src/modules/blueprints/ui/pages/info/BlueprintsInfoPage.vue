44ce7d785fa02092245c29f41f92ce7b7fe28e7a
Natan <natanvnascimento@gmail.com>
2025-12-18 17:21:07 -0300

    refactor(web/blueprints): simplify BlueprintInfoPage by removing unused error handling and improving reactivity handling

--- web/src/modules/blueprints/ui/pages/info/BlueprintsInfoPage.vue
+++ web/src/modules/blueprints/ui/pages/info/BlueprintsInfoPage.vue
@@ -1,13 +1,6 @@
 <template>
-    <Resource
-        :resource="resource"
-        :reactivity-key="blueprint"
-        @loaded="(value: unknown) => (blueprint = value)"
-        @error="onError"
-    >
+    <Resource :resource="resource" @loaded="(value) => (blueprint = value)">
         <div v-if="blueprint">Blueprint {{ blueprint.spec.name }}</div>
-        <div v-else-if="errors.notFound">Não encontrado</div>
-        <ErrorState v-else />
     </Resource>
 </template>
 <script setup lang="ts">
@@ -15,20 +8,9 @@ import { ref } from "vue"
 import type { Blueprint } from "@/modules/blueprints/api/models/blueprint.model"
 import blueprintsService from "@/modules/blueprints/api/services/blueprints.service"
 import Resource from "@/modules/platform/ui/components/Resource.vue"
-import ErrorState from "@/modules/platform/ui/components/ErrorState.vue"
-import { HttpError, HttpErrors } from "@/modules/platform/api/models/error.model"
 
 const props = defineProps<{ blueprintId: string }>()
-defineEmits(["refresh"])
 
-// State
 const resource = () => blueprintsService.getBlueprint(props.blueprintId)
-const blueprint: Blueprint = ref(null)
-const errors = ref({ notFound: false })
-
-// Functions
-function onError(error: Error): void {
-    errors.value.notFound =
-        error instanceof HttpError && (error as HttpError).code === HttpErrors.UNKNOWN_BLUEPRINT
-}
+const blueprint = ref<Blueprint | null>(null)
 </script>
