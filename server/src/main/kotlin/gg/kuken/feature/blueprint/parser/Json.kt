b1fb3270fe3d862b90b44ca4a792fcba43db7789
Natan <natanvnascimento@gmail.com>
2025-12-18 16:57:43 -0300

    fix(server/blueprints): handle missing JSON fields gracefully with `contentOrNull`

--- server/src/main/kotlin/gg/kuken/feature/blueprint/parser/Json.kt
+++ server/src/main/kotlin/gg/kuken/feature/blueprint/parser/Json.kt
@@ -1,9 +1,10 @@
 package gg.kuken.feature.blueprint.parser
 
 import kotlinx.serialization.json.JsonObject
+import kotlinx.serialization.json.contentOrNull
 import kotlinx.serialization.json.jsonObject
 import kotlinx.serialization.json.jsonPrimitive
 
-fun JsonObject.string(key: String) = getValue(key).jsonPrimitive.content
+fun JsonObject.string(key: String) = getValue(key).jsonPrimitive.contentOrNull.orEmpty()
 
 fun JsonObject.struct(key: String) = this[key]?.jsonObject
