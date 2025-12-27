39d30d4a4b08ab38c00197f828f80861189e4bcc
Natan <natanvnascimento@gmail.com>
2025-12-27 12:55:19 -0300

    feat(web/instances): add `InstanceConsolePage` component to request and display instance logs

---
+++ web/src/modules/instances/ui/pages/InstanceConsolePage.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import websocketService from "@/modules/platform/api/services/websocket.service.ts"
+import { WebSocketOpCodes } from "@/modules/platform/api/models/websocket.response.ts"
+
+const props = defineProps<{ instanceId: string }>()
+
+websocketService.send(WebSocketOpCodes.InstanceLogsRequest, {
+    iid: props.instanceId
+})
+</script>
+
+<template>Pirocoptero 333</template>
+
+<style scoped lang="scss"></style>
