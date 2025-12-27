c79c369261a332e6912a3d74f8ffc5785226fb09
Natan <natanvnascimento@gmail.com>
2025-12-27 19:37:42 -0300

    feat(web/websocket): add new opcodes for detailed instance logs request handling

--- web/src/modules/platform/api/models/websocket.response.ts
+++ web/src/modules/platform/api/models/websocket.response.ts
@@ -1,6 +1,10 @@
 export const WebSocketOpCodes = {
     InstanceUnavailable: 1 as const,
     InstanceLogsRequest: 2 as const,
+    InstanceLogsRequestStarted: 3 as const,
+    InstanceLogsRequestFrame: 4 as const,
+    InstanceLogsRequestFinished: 5 as const,
+
 } as const
 
 export type WebSocketOp = (typeof WebSocketOpCodes)[keyof typeof WebSocketOpCodes]
