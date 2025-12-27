d838928d03ac0d2941d537fb802da0462a3b92c2
Natan <natanvnascimento@gmail.com>
2025-12-27 17:50:27 -0300

    feat(server/websocket): add new opcodes for detailed instance logs request handling

--- server/src/main/kotlin/gg/kuken/http/websocket/WebSocketOpCodes.kt
+++ server/src/main/kotlin/gg/kuken/http/websocket/WebSocketOpCodes.kt
@@ -7,4 +7,10 @@ object WebSocketOpCodes {
     const val InstanceUnavailable: WebSocketOp = 1
 
     const val InstanceLogsRequest: WebSocketOp = 2
+
+    const val InstanceLogsRequestStarted: WebSocketOp = 3
+
+    const val InstanceLogsRequestFrame: WebSocketOp = 4
+
+    const val InstanceLogsRequestFinished: WebSocketOp = 5
 }
