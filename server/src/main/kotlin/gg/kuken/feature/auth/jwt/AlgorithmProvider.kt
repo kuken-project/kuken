45e4d48e55e50b2727c1d2446344ecd661d7a55a
Natan <natanvnascimento@gmail.com>
2025-12-28 19:35:51 -0300

    refactor(server/auth): rename `AlgorithmFactory` to `AlgorithmProvider` and optimize algorithm initialization

--- server/src/main/kotlin/gg/kuken/feature/auth/jwt/AlgorithmProvider.kt
+++ server/src/main/kotlin/gg/kuken/feature/auth/jwt/AlgorithmProvider.kt
@@ -2,12 +2,14 @@ package gg.kuken.feature.auth.jwt
 
 import com.auth0.jwt.algorithms.Algorithm
 
-class AlgorithmFactory {
-    fun create(): Algorithm {
+class AlgorithmProvider {
+    private val algorithm by lazy {
         val secret =
             System.getenv("JWT_SECRET") ?: java.util.UUID
                 .randomUUID()
                 .toString()
-        return Algorithm.HMAC256(secret)
+        Algorithm.HMAC256(secret)
     }
+
+    fun get(): Algorithm = algorithm
 }
