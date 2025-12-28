00e403b557ee8bd3a8038d9c718c510ea9177480
Natan <natanvnascimento@gmail.com>
2025-12-28 18:40:50 -0300

    feat(server/instance): add permission check for getInstanceDetails endpoint

--- server/src/main/kotlin/gg/kuken/feature/instance/http/Find.kt
+++ server/src/main/kotlin/gg/kuken/feature/instance/http/Find.kt
@@ -1,14 +1,22 @@
 package gg.kuken.feature.instance.http
 
 import gg.kuken.feature.instance.InstanceService
+import gg.kuken.feature.rbac.Permissions
+import gg.kuken.feature.rbac.http.requirePermission
 import gg.kuken.http.util.validateOrThrow
 import io.ktor.server.resources.get
 import io.ktor.server.response.respond
 import io.ktor.server.routing.Route
+import io.ktor.server.util.getOrFail
 import jakarta.validation.Validator
 import org.koin.ktor.ext.inject
+import kotlin.uuid.Uuid
 
 fun Route.getInstanceDetails() {
+    requirePermission(Permissions.ReadInstance) { call ->
+        call.parameters.getOrFail("instanceId").let(Uuid::parse)
+    }
+
     val instanceService by inject<InstanceService>()
     val validator by inject<Validator>()
 
