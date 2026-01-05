13b7b489eb1f153232dcca6ab4cc44a1113aa179
Natan <natanvnascimento@gmail.com>
2026-01-05 12:39:15 -0300

    feat(web): extend Unit model with detailed instance info

--- web/src/modules/units/api/models/unit.model.ts
+++ web/src/modules/units/api/models/unit.model.ts
@@ -1,5 +1,24 @@
 export type Unit = {
     id: string
     name: string
-    instance: string
+    instance: UnitInstance
+    created: string
+    updated: string
+    status: string
+}
+
+export type UnitInstance = {
+    id: string
+    address: UnitInstanceAddress
+    blueprint: UnitInstanceBlueprint
+}
+
+export type UnitInstanceAddress = {
+    host: string
+    port: string
+}
+
+export type UnitInstanceBlueprint = {
+    id: string
+    iconUrl?: string
 }
