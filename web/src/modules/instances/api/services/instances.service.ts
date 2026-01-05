ab430c6ef9f7d5910f7e9178b1a46e88738dfae8
Natan <natanvnascimento@gmail.com>
2026-01-05 12:40:12 -0300

    feat(web): add `runInstanceCommand` method to InstancesService

--- web/src/modules/instances/api/services/instances.service.ts
+++ web/src/modules/instances/api/services/instances.service.ts
@@ -2,12 +2,16 @@ import httpService from "@/modules/platform/api/services/http.service.ts"
 import type { AxiosResponse } from "axios"
 import type { Instance } from "@/modules/instances/api/models/instance.model.ts"
 
-class InstancesService {
+export default {
     async getInstance(instanceId: String): Promise<Instance> {
         return httpService
             .get(`instances/${instanceId}`)
             .then((res: AxiosResponse) => res.data as Instance)
-    }
-}
+    },
 
-export default () => new InstancesService()
+    async runInstanceCommand(instanceId: String, command: string): Promise<{ exitCode: number }> {
+        return httpService
+            .post(`instances/${instanceId}/command`, { command })
+            .then((res: AxiosResponse) => res.data)
+    }
+} as const
