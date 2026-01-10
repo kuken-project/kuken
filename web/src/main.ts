a7ef63cfeb12e27abee638eac87f001716775600
Natan <natanvnascimento@gmail.com>
2026-01-10 16:26:19 -0300

    Setup `@unhead/vue`

--- web/src/main.ts
+++ web/src/main.ts
@@ -7,12 +7,14 @@ import { createPinia } from "pinia"
 import VueProgressiveImage from "vue-progressive-image"
 import logService from "@/modules/platform/api/services/log.service"
 import configService from "@/modules/platform/api/services/config.service"
+import { createHead } from "@unhead/vue";
 
 createApp(App)
     .use(createVfm())
     .use(createPinia())
     .use(VueProgressiveImage)
     .use(router)
+    .use(createHead())
     .mount("#app")
 
 logService.info(configService.toVersionInfoString())
