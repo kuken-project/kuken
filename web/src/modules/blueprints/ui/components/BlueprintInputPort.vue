4ae6923bc9a84a582f64a678da006187a89a6e6a
Natan <natanvnascimento@gmail.com>
2026-01-14 02:42:53 -0300

    Create BlueprintInputPort

---
+++ web/src/modules/blueprints/ui/components/BlueprintInputPort.vue
@@ -0,0 +1,25 @@
+<script lang="ts" setup>
+import VInput from "@/modules/platform/ui/components/form/VInput.vue"
+import VLabel from "@/modules/platform/ui/components/form/VLabel.vue"
+import VFieldSet from "@/modules/platform/ui/components/form/VFieldSet.vue"
+import { onMounted } from "vue"
+import type { BlueprintBuildInput } from "@/modules/blueprints/api/models/blueprint.model.ts"
+
+const props = defineProps<Partial<BlueprintBuildInput>>()
+const model = defineModel()
+
+onMounted(() => {
+    model.value = props.default
+})
+</script>
+
+<template>
+    <VFieldSet>
+        <VLabel>
+            {{ props.label }}
+            <VInput :placeholder="props.placeholder" :value="model" required="true" type="number" />
+        </VLabel>
+    </VFieldSet>
+</template>
+
+<style lang="scss" scoped></style>
