becc3fd5ef38e7dbd826c6d41013cd51766c5092
Natan <natanvnascimento@gmail.com>
2026-01-15 19:15:21 -0300

    Update Hytale blueprint

---
+++ blueprints/blueprints/games/hytale/install.sh
@@ -0,0 +1,18 @@
+#!/bin/ash
+set -e
+
+mkdir -p /mnt/server
+cd /mnt/server
+
+DOWNLOAD_URL="https://downloader.hytale.com/hytale-downloader.zip"
+
+echo -e "Downloading Hytale Downloader CLI..."
+rm -f hytale-downloader.zip
+curl -o hytale-downloader.zip $DOWNLOAD_URL
+unzip -o hytale-downloader.zip -d hytale-downloader
+mv hytale-downloader/hytale-downloader-linux-amd64 hytale-downloader/hytale-downloader-linux
+chmod 555 hytale-downloader/hytale-downloader-linux
+
+echo -e "Verifying Hytale Downloader installation..."
+echo -e "Hytale Downloader version: $(/hytale-downloader/hytale-downloader-linux -version)"
+echo -e "Hytale Downloader installed successfully."
