7a7e7cc1144180c693d22b09e132b07c1fba3c4b
Natan <natanvnascimento@gmail.com>
2026-01-20 01:07:16 -0300

    Close WS connection on App unmount

--- web/src/App.vue
+++ web/src/App.vue
@@ -7,13 +7,14 @@
 <script lang="ts" setup>
 import { ModalsContainer } from "vue-final-modal"
 import LoadingState from "@/modules/platform/ui/components/LoadingState.vue"
-import { ref } from "vue"
+import { onUnmounted, ref } from "vue"
 import backendService from "@/modules/platform/api/services/backend.service.ts"
 import { useHead } from "@unhead/vue"
 import configService from "@/modules/platform/api/services/config.service.ts"
 import { SETUP_ROUTE } from "@/router.ts"
 import { useRouter } from "vue-router"
 import { usePlatformStore } from "@/modules/platform/platform.store.ts"
+import websocketService from "@/modules/platform/api/services/websocket.service.ts"
 
 const isLoading = ref(true)
 
@@ -34,6 +35,8 @@ backendService
             isLoading.value = false
         }
     })
+
+onUnmounted(() => websocketService.close())
 </script>
 <style lang="scss">
 #app {
