181f7b890e5d705b7d83973d2764fe438978fc74
Natan <natanvnascimento@gmail.com>
2026-02-02 20:07:02 -0300

    Move data directory to Kuken config

--- server/src/main/kotlin/gg/kuken/KukenConfig.kt
+++ server/src/main/kotlin/gg/kuken/KukenConfig.kt
@@ -1,13 +1,11 @@
 package gg.kuken
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
-import java.io.File
 import java.nio.file.Files
 import java.nio.file.Path
-import java.nio.file.Paths
+import kotlin.io.path.Path
 import kotlin.io.path.createDirectories
-import kotlin.io.path.createDirectory
-import kotlin.io.path.createParentDirectories
 
 @Serializable
 data class KukenConfig(
@@ -16,8 +14,18 @@ data class KukenConfig(
     val redis: RedisConfig,
     val node: String,
     val docker: DockerConfig,
+    val engine: EngineConfig,
 ) {
     val devMode: Boolean = System.getenv("DEV_MODE")?.toBooleanStrictOrNull() == true
+
+    @Serializable
+    data class EngineConfig(
+        @SerialName("data-directory")
+        private val _dataDirectory: String,
+    ) {
+        val dataDirectory get() = Path(_dataDirectory)
+        val instancesDataDirectory get() = dataDirectory.resolve("instances")
+    }
 
     @Serializable
     data class HttpConfig(
@@ -40,6 +48,7 @@ data class KukenConfig(
     @Serializable
     data class DockerConfig(
         val network: Network,
+        @SerialName("api-version") val apiVersion: String,
     ) {
         @Serializable
         data class Network(
